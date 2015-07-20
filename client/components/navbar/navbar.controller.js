56d685dfe12c0f6c71845bb10518cffc11038c13
Dennis Ting <dennisting@gmail.com>
2015-07-20 00:15:56 -0700

    Update navbar.

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -1,15 +1,4 @@
 'use strict';
 
-angular.module('fccstockjumpApp')
-  .controller('NavbarCtrl', function ($scope, $location) {
-    $scope.menu = [{
-      'title': 'Home',
-      'link': '/'
-    }];
-
-    $scope.isCollapsed = true;
-
-    $scope.isActive = function(route) {
-      return route === $location.path();
-    };
-  });
+angular.module('fccstockjumpApp').controller('NavbarCtrl', function() {
+});
